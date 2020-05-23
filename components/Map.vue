9beb839e13be8652daac47b8594415c973077322
enghamzasalem <enghamzasalem@yahoo.com>
2020-05-24 00:54:07 +0300

    add all files

---
+++ components/Map.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="/w3images/map.jpg" class="w3-image w3-greyscale" style="width:100%;" id="myMap" />
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <NavBar />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import NavBar from "~/components/NavBar.vue";
+import Footer from "~/components/Footer.vue";
+
+export default {
+  components: {
+    NavBar,
+    Footer
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+body,
+html {
+  height: 100%;
+}
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Amatic SC", sans-serif;
+}
+.menu {
+  display: none;
+}
+.bgimg {
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url("https://www.w3schools.com/w3images/pizza.jpg");
+  min-height: 90%;
+  height: 100vh;
+}
+</style>
